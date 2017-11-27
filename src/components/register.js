d9071dda99b21a4c272d2e116d3de4c212543833
CK Wagaba <ckwagaba@gmail.com>
2017-11-27 15:41:11 +0300

    add main components

---
+++ src/components/register.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Register extends Component {
+  render () {
+    return <input />;
+  }
+}
+
+export default Register;
