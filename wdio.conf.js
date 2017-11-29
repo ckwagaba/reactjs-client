bbe01c27f36c3fe1b9b2e6cd3a8c149be74067ed
CK Wagaba <ckwagaba@gmail.com>
2017-11-29 12:43:35 +0300

    change e2e tests dir

--- wdio.conf.js
+++ wdio.conf.js
@@ -10,7 +10,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/specs/**/*.js'
+        './src/e2e_tests/**/*.js'
     ],
     // Patterns to exclude.
     exclude: [
