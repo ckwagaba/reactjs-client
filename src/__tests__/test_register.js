14c82f9b24d6a9bce26a3f7c21e9be158f3ed282
CK Wagaba <ckwagaba@gmail.com>
2017-12-14 11:21:47 +0300

    test existence of child components

--- src/__tests__/test_register.js
+++ src/__tests__/test_register.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Register from '../components/register.js';
+import Register from '../components/register';
 
 describe('Register component', () => {
 
   const component = shallow(<Register />);
 
-  it('renders without crashing', () => {
+  it('should exist', () => {
     expect(component.exists()).toEqual(true);
   });
 
-  it('should have a text input', () => {
-    expect(component.find('.email_field').length).toEqual(1);
+  it('should have a text field', () => {
+    expect(component.find('TextInput').length).toEqual(1);
+  });
+
+  it('should have a password field', () => {
+    expect(component.find('PasswordInput').length).toEqual(1);
+  });
+
+  it('should have a submit button', () => {
+    expect(component.find('SubmitButton').length).toEqual(1);
   });
 
 });
