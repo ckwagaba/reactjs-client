f737a21eb3b39c242797c1ab6486208fc6ca0eb9
CK Wagaba <ckwagaba@gmail.com>
2017-12-29 22:30:17 +0300

    make component stateful

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './css/main.css';
 import RegisterView from './components/register_view';
 //import LoginView from './components/login_view';
 import registerServiceWorker from './registerServiceWorker';
 
-const App = () => {
-  return (
-    <div>
-      <RegisterView />
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <RegisterView />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
