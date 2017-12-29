da51fd37e004f7e04e4d3dfd382cdff15a96f055
CK Wagaba <ckwagaba@gmail.com>
2017-12-29 15:56:34 +0300

    create login view

---
+++ src/components/login_view.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PagePurpose from './page_purpose';
+import AppDetails from './app_details';
+import EmailInput from './email_input';
+import SubmitButton from './submit_button';
+import PasswordInput from './password_input';
+import AltOption from './alt_option';
+
+const LoginView = () => {
+  return (
+    <div className="landing_page login_view">
+      <AppDetails />
+      <div className="form_section">
+        <PagePurpose purpose="Access your account" />
+        <form>
+          <EmailInput placeholder="Email Address" />
+          <PasswordInput placeholder="Enter Password" />
+          <SubmitButton buttonText="Sign In" />
+        </form>
+        <AltOption altOptionLink="#" altOptionText="Sign Up" />
+      </div>
+    </div>
+  );
+}
+
+export default LoginView;
