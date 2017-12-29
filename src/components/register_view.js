ef1172337925dea4ef271355e87cd5ce4aa682f3
CK Wagaba <ckwagaba@gmail.com>
2017-12-29 15:56:56 +0300

    refactor register view

---
+++ src/components/register_view.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PagePurpose from './page_purpose';
+import AppDetails from './app_details';
+import EmailInput from './email_input';
+import SubmitButton from './submit_button';
+import PasswordInput from './password_input';
+import AltOption from './alt_option';
+
+const RegisterView = () => {
+  return (
+    <div className="landing_page register_view">
+      <AppDetails />
+      <div className="form_section">
+        <PagePurpose purpose="Create your account" />
+        <form>
+          <EmailInput placeholder="Email Address" />
+          <PasswordInput placeholder="New Password" />
+          <PasswordInput placeholder="Confirm Password" />
+          <SubmitButton buttonText="Sign Up" />
+        </form>
+        <AltOption altOptionLink="#" altOptionText="Sign In" />
+      </div>
+    </div>
+  );
+}
+
+export default RegisterView;
