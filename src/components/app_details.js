ac2fc4a05913a6bd670fb9c5220bf693c212b898
CK Wagaba <ckwagaba@gmail.com>
2017-12-29 15:59:07 +0300

    add app details

---
+++ src/components/app_details.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AppDetails = () => {
+  return (
+    <div className="app_details">
+      <div className="app_name">
+        Bucketlist
+      </div>
+      <div className="app_description">
+        Track Your Dreams
+      </div>
+    </div>
+  );
+}
+
+export default AppDetails;
