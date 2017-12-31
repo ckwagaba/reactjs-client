0f90c0a3317a38e9828e8d8ab9c866f8382f795a
CK Wagaba <ckwagaba@gmail.com>
2017-12-31 17:55:32 +0300

    implement routing

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import '../css/main.css';
 import RegisterView from './register_view';
 import LoginView from './login_view';
+import BucketlistView from './bucketlist_view';
+import ItemView from './item_view';
 
 class App extends Component {
   state = {
@@ -11,7 +14,21 @@ class App extends Component {
   render () {
     return (
       <div>
-        {this.state.loggedIn ? <RegisterView /> : <LoginView />}
+        {
+          this.state.loggedIn ?
+          <Switch>
+            <Route exact path='/' component={BucketlistView} />
+            <Route path='/bucketlists/' component={BucketlistView} />
+            <Route path='/bucketlists/:bucketlist_id' component={BucketlistView} />
+            <Route path='/bucketlists/:bucketlist_id/items/' component={ItemView} />
+            <Route path='/bucketlists/:bucketlist_id/items/:item_id' component={ItemView} />
+          </Switch> :
+          <Switch>
+            <Route exact path='/' component={RegisterView} />
+            <Route path='/auth/register' component={RegisterView} />
+            <Route path='/auth/login' component={LoginView} />
+          </Switch>
+        }
       </div>
     );
   }
