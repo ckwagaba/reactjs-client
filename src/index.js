a8cc57f8500d536ef233f1c392869b74c288eac5
CK Wagaba <ckwagaba@gmail.com>
2017-12-31 15:54:49 +0300

    playing with state

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './css/main.css';
 import RegisterView from './components/register_view';
-//import LoginView from './components/login_view';
+import LoginView from './components/login_view';
 import registerServiceWorker from './registerServiceWorker';
 
 class App extends Component {
+  state = {
+    loggedIn: false
+  }
+
   render () {
     return (
       <div>
-        <RegisterView />
+        {this.state.loggedIn ? <RegisterView /> : <LoginView />}
       </div>
     );
   }
