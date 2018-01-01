67ff6ab48002af66bbb141fa38a1e27da7166f28
CK Wagaba <ckwagaba@gmail.com>
2018-01-01 12:56:36 +0300

    add bucketlist view component

---
+++ src/components/bucketlist_view.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BucketlistView = () => {
+  return (
+    <div className="landing_page bucketlist_view">
+      
+    </div>
+  );
+}
+
+export default BucketlistView;
