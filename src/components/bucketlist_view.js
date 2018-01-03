13c8f88dc8847a1883fe694071c5345b454bf52f
CK Wagaba <ckwagaba@gmail.com>
2018-01-03 12:49:55 +0300

    refactor for dynamic content

--- src/components/bucketlist_view.js
+++ src/components/bucketlist_view.js
@@ -2,13 +2,28 @@ import React from 'react';
 import Nav from './nav';
 import Header from './header';
 import Main from './main';
+import Bucketlist from './bucketlist';
+
+const listToRender = [
+  {name: 'Career'},
+  {name: 'Family'},
+  {name: 'Lifestyle'},
+  {name: 'Spiritual'},
+  {name: 'Music'},
+  {name: 'Business'},
+  {name: 'Leadership'},
+  {name: 'Relationship'}
+]
 
 const BucketlistView = () => {
+  const rows = listToRender.map((item) =>
+    <Bucketlist key={item.name} bucketlistName={item.name} />
+  );
   return (
     <div className="landing_page bucketlist_view">
       <Nav />
       <Header currentLocation="Bucketlist" />
-      <Main />
+      <Main listToRender={rows} />
     </div>
   );
 }
--- src/components/main.js
+++ src/components/main.js
@@ -1,18 +1,10 @@
 import React, { Component } from 'react';
-import Bucketlist from './bucketlist';
 
 class Main extends Component {
   render () {
     return (
       <main>
-        <Bucketlist bucketlistName="Career" />
-        <Bucketlist bucketlistName="Family" />
-        <Bucketlist bucketlistName="Lifestyle" />
-        <Bucketlist bucketlistName="Health" />
-        <Bucketlist bucketlistName="BucketlistName" />
-        <Bucketlist bucketlistName="BucketlistName" />
-        <Bucketlist bucketlistName="BucketlistName" />
-        <Bucketlist bucketlistName="BucketlistName" />
+        {this.props.listToRender}
       </main>
     );
   }
