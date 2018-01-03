3eed6fd323d236fa6bbe8f7ef850d1481d4721d5
CK Wagaba <ckwagaba@gmail.com>
2018-01-04 00:20:11 +0300

    add state

--- src/components/app.js
+++ src/components/app.js
@@ -7,15 +7,19 @@ import BucketlistView from './bucketlist_view';
 import ItemView from './item_view';
 
 class App extends Component {
-  state = {
-    loggedIn: true
+  constructor (props) {
+    super(props);
+    this.state = {
+      accessToken: ''
+    }
   }
 
   render () {
     return (
       <div>
         {
-          this.state.loggedIn ?
+          this.state.accessToken
+          ?
           <Switch>
             <Route exact path='/' component={BucketlistView} />
             <Route path='/bucketlists/' component={BucketlistView} />
@@ -23,7 +27,8 @@ class App extends Component {
             <Route path='/items/' component={ItemView} />
             <Route path='/bucketlists/:bucketlist_id/items/' component={ItemView} />
             <Route path='/bucketlists/:bucketlist_id/items/:item_id' component={ItemView} />
-          </Switch> :
+          </Switch>
+          :
           <Switch>
             <Route exact path='/' component={RegisterView} />
             <Route path='/auth/register' component={RegisterView} />
