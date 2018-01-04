01745d3fae4521a6eb4c3259da7883b7dcb1b956
CK Wagaba <ckwagaba@gmail.com>
2018-01-04 10:35:54 +0300

    imporve readability

--- src/components/app.js
+++ src/components/app.js
@@ -15,28 +15,28 @@ class App extends Component {
   }
 
   render () {
-    return (
-      <div>
-        {
-          this.state.accessToken
-          ?
-          <Switch>
-            <Route exact path='/' component={BucketlistView} />
-            <Route path='/bucketlists/' component={BucketlistView} />
-            <Route path='/bucketlists/:bucketlist_id' component={BucketlistView} />
-            <Route path='/items/' component={ItemView} />
-            <Route path='/bucketlists/:bucketlist_id/items/' component={ItemView} />
-            <Route path='/bucketlists/:bucketlist_id/items/:item_id' component={ItemView} />
-          </Switch>
-          :
-          <Switch>
-            <Route exact path='/' component={RegisterView} />
-            <Route path='/auth/register' component={RegisterView} />
-            <Route path='/auth/login' component={LoginView} />
-          </Switch>
-        }
-      </div>
-    );
+    if (this.state.accessToken) {
+      return (
+        <Switch>
+          <Route exact path='/' component={BucketlistView} />
+          <Route path='/bucketlists/' component={BucketlistView} />
+          <Route path='/bucketlists/:bucketlist_id' component={BucketlistView} />
+          <Route path='/items/' component={ItemView} />
+          <Route path='/bucketlists/:bucketlist_id/items/' component={ItemView} />
+          <Route path='/bucketlists/:bucketlist_id/items/:item_id' component={ItemView} />
+        </Switch>
+      );
+    }
+
+    else {
+      return (
+        <Switch>
+          <Route exact path='/' component={RegisterView} />
+          <Route path='/auth/register' component={RegisterView} />
+          <Route path='/auth/login' component={LoginView} />
+        </Switch>
+      );
+    }
   }
 }
 
