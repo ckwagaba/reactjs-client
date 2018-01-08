538d06c91011a6b2d16430e1bd51ead9d9ec7930
CK Wagaba <ckwagaba@gmail.com>
2018-01-08 14:27:46 +0300

    refactor

--- src/components/bucketlist_view.js
+++ src/components/bucketlist_view.js
@@ -1,21 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Nav from './nav';
 import Header from './header';
 import Main from './main';
 import Bucketlist from './bucketlist';
 
-const BucketlistView = (props) => {
-  const rows = props.listToRender.map((item) =>
-    <Bucketlist key={item.name} bucketlistName={item.name} bucketlistId={item.id} />
-  );
+class BucketlistView extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      listToRender: []
+    }
+  }
 
-  return (
-    <div className="landing_page bucketlist_view">
-      <Nav handleLogout={props.handleLogout} />
-      <Header currentLocation="Bucketlist" itemForm='/createbucketlist' />
-      <Main componentToRender={rows} />
-    </div>
-  );
+  componentDidMount() {
+    this.getBucketlists();
+  }
+
+  // get bucketlists
+  getBucketlists = () => {
+    fetch('http://127.0.0.1:5000/v1/bucketlists/', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': this.props.ACCESSTOKEN
+      }
+    })
+    .then(response => {
+      return response.json()
+    })
+    .then(responseData => {
+      this.setState({
+        listToRender: responseData.bucketlists_on_page
+      });
+    });
+  }
+
+  render () {
+    const rows = this.state.listToRender.map((item) =>
+      <Bucketlist key={item.name} bucketlistName={item.name} bucketlistId={item.id} />
+    );
+
+    return (
+      <div className="landing_page bucketlist_view">
+        <Nav handleLogout={this.props.handleLogout} />
+        <Header currentLocation="Bucketlist" itemForm='/createbucketlist' />
+        <Main componentToRender={rows} />
+      </div>
+    );
+  }
 }
 
 export default BucketlistView;
