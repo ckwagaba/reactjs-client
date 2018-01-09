d56fdacfad57a8f9f4760fb4060f605010b06215
CK Wagaba <ckwagaba@gmail.com>
2018-01-09 07:17:17 +0300

    handle deletion

--- src/components/bucketlist.js
+++ src/components/bucketlist.js
@@ -3,6 +3,24 @@ import Button from './button';
 import { Link } from 'react-router-dom';
 
 const Bucketlist = (props) => {
+  // handle bucketlist delete
+  const handleDelete = (bucketlistId) => {
+    console.log('c');
+    fetch('http://127.0.0.1:5000/v1/bucketlists/' + props.bucketlistId, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.getItem('ACCESSTOKEN')
+      }
+    })
+    .then(response => {
+      return response.json()
+    })
+    .then(responseData => {
+      // on successful delete: re-render BucketlistView component
+    });
+  }
+
   return (
     <div className="bucketlist">
       <Link to={"/bucketlists/" + props.bucketlistId + "/items/"} className="bucketlist_thumbnail">
@@ -10,7 +28,7 @@ const Bucketlist = (props) => {
       </Link>
       <div className="bucketlist_actions">
         <Button buttonText="Edit" className="edit_button" />
-        <Button buttonText="Delete" className="delete_button" />
+        <Button buttonText="Delete" className="delete_button" onClick={() => handleDelete(props.bucketlistId)} />
       </div>
     </div>
   );
--- src/components/item.js
+++ src/components/item.js
@@ -2,13 +2,31 @@ import React from 'react';
 import Button from './button';
 
 const Item = (props) => {
+  // handle item delete
+  const handleDelete = (bucketlistId) => {
+    console.log('c');
+    fetch('http://127.0.0.1:5000/v1/bucketlists/' + props.bucketlistId + '/items/' + props.itemId, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.getItem('ACCESSTOKEN')
+      }
+    })
+    .then(response => {
+      return response.json()
+    })
+    .then(responseData => {
+      // on successful delete: re-render ItemtView component
+    });
+  }
+
   return (
     <div className="item">
       <span className="item_name">{props.itemName}</span>
       <span className="item_bucket">{props.itemBucket}</span>
       <span className="item_actions">
         <Button buttonText="Edit" className="edit_button" />
-        <Button buttonText="Delete" className="delete_button" />
+        <Button buttonText="Delete" className="delete_button" onClick={() => handleDelete(props.itemId)} />
       </span>
     </div>
   );
