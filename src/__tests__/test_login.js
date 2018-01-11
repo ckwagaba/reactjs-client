b26eb601759e722d2a8914fc839944b507db7764
CK Wagaba <ckwagaba@gmail.com>
2018-01-11 11:41:02 +0300

    remove useless tests

--- src/__tests__/test_login.js
+++ src/__tests__/test_login.js
@@ -10,32 +10,4 @@ describe('Register component', () => {
     expect(component.exists()).toEqual(true);
   });
 
-  it('should have an email field', () => {
-    expect(component.find('EmailInput').length).toEqual(1);
-  });
-
-  it('should have a password field', () => {
-    expect(component.find('PasswordInput').length).toEqual(1);
-  });
-
-  it('should have a submit button', () => {
-    expect(component.find('SubmitButton').length).toEqual(1);
-  });
-
-  it('should have app details', () => {
-    expect(component.find('AppDetails').length).toEqual(1);
-  });
-
-  it('should have page purpose', () => {
-    expect(component.find('PagePurpose').length).toEqual(1);
-  });
-
-  it('should have a server response', () => {
-    expect(component.find('ServerResponse').length).toEqual(1);
-  });
-
-  it('should have an alternative option', () => {
-    expect(component.find('AltOption').length).toEqual(1);
-  });
-
 });
