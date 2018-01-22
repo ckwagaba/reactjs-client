b0032fbae7e627fc82abd7605f9a830974b173c6
CK Wagaba <ckwagaba@gmail.com>
2018-01-22 15:01:58 +0300

    creator container dir and update routes

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import '../css/main.css';
-import RegisterView from './register_view';
-import LoginView from './login_view';
-import BucketlistView from './bucketlist_view';
-import ItemView from './item_view';
-import BucketlistForm from './bucketlist_form';
-import ItemForm from './item_form';
+import RegisterView from '../components/register_view';
+import LoginView from '../components/login_view';
+import BucketlistView from '../components/bucketlist_view';
+import ItemView from '../components/item_view';
+import BucketlistForm from '../components/bucketlist_form';
+import ItemForm from '../components/item_form';
 
 const App = (props) =>  {
     // access protected routes: apparently localStorage does not accept BOOLEANS
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
-import App from './components/app';
+import App from './containers/app';
 
 ReactDOM.render(
   (
