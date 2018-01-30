07d05814563f74c02c8bd2c7084e648aed2bed00
CK Wagaba <ckwagaba@gmail.com>
2018-01-31 01:06:03 +0300

    create bucketlist reducer

---
+++ src/reducers/BucketlistReducer.js
@@ -0,0 +1,43 @@
+const initialState = {
+  listToRender: [],
+  searchTerm: '',
+  totalItems: 0,
+  limit: 5,
+  currentPage: 1,
+  totalPages: 1
+};
+
+const bucketlistReducer = (state=initialState, action) => {
+  switch (action.type) {
+    case 'STORE_BUCKETLISTS':
+      return {
+        ...state,
+        listToRender: action.payload.listToRender,
+        totalPages: action.payload.totalPages
+      }
+
+    case 'SET_TOTAL_BUCKETLISTS':
+      return {
+        ...state,
+        totalItems: action.payload
+      }
+
+    case 'SET_CURRENT_PAGE':
+      return {
+        ...state,
+        currentPage: action.payload
+      }
+
+      case 'SET_SEARCH_TERM':
+        return {
+          ...state,
+          searchTerm: action.payload.searchTerm,
+          currentPage: action.payload.currentPage
+        }
+
+    default:
+      return state;
+  }
+}
+
+export default bucketlistReducer;
