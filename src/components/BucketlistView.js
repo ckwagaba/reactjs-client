89487e48d2d30d6ed3d7902da0cebeada67a1aeb
CK Wagaba <ckwagaba@gmail.com>
2018-01-31 01:02:54 +0300

    refactor for redux

--- src/components/BucketlistView.js
+++ src/components/BucketlistView.js
@@ -1,24 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Nav from './Nav';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 import Bucketlist from './Bucketlist';
 import { BASEURL } from '../Config.js';
+import store from '../store/Store';
 
 class BucketlistView extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      listToRender: [],
-      searchTerm: '',
-      totalItems: 0,
-      limit: 5,
-      currentPage: 1,
-      totalPages: 1
-    }
-  }
-
   // initial component render
   componentDidMount() {
     // get as required
@@ -26,24 +16,25 @@ class BucketlistView extends Component {
   }
 
   // when component re-renders
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(prevProps) {
     // if the current page changes, or the search term changes.
-    if(prevState.currentPage !== this.state.currentPage || prevState.searchTerm !== this.state.searchTerm) {
+    if(prevProps.bucketlistState.currentPage !== this.props.bucketlistState.currentPage || prevProps.bucketlistState.searchTerm !== this.props.bucketlistState.searchTerm) {
       this.getBucketlists();
     }
   }
 
   // update currentPage
   setCurrentPage = (currentPage) => {
-    this.setState({
-      currentPage: currentPage
+    store.dispatch({
+      type: 'SET_CURRENT_PAGE',
+      payload: currentPage
     });
   }
 
   // get bucketlists
   getBucketlists = () => {
     this.getTotalItems();
-    fetch(BASEURL + '/bucketlists/?q=' + this.state.searchTerm + '&limit=' + this.state.limit + '&page=' + this.state.currentPage, {
+    fetch(BASEURL + '/bucketlists/?q=' + this.props.bucketlistState.searchTerm + '&limit=' + this.props.bucketlistState.limit + '&page=' + this.props.bucketlistState.currentPage, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -54,16 +45,19 @@ class BucketlistView extends Component {
       return response.json()
     })
     .then(responseData => {
-      this.setState({
-        listToRender: responseData.bucketlists_on_page,
-        totalPages: Math.ceil(this.state.totalItems / this.state.limit)
+      store.dispatch({
+        type: 'STORE_BUCKETLISTS',
+        payload: {
+          listToRender: responseData.bucketlists_on_page,
+          totalPages: Math.ceil(this.props.bucketlistState.totalItems / this.props.bucketlistState.limit)
+        }
       });
     });
   }
 
   // get total items
   getTotalItems = () => {
-    fetch(BASEURL + '/bucketlists/?q=' + this.state.searchTerm, {
+    fetch(BASEURL + '/bucketlists/?q=' + this.props.bucketlistState.searchTerm, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -74,45 +68,40 @@ class BucketlistView extends Component {
       return response.json()
     })
     .then(responseData => {
-      this.setState({
-        totalItems: responseData.number_of_bucketlists_on_page
+      store.dispatch({
+        type: 'SET_TOTAL_BUCKETLISTS',
+        payload: responseData.number_of_bucketlists_on_page
       });
     });
   }
 
   // search API for items
   handleSearch = (event) => {
-    /*
-    NOTES
-    Neither Search nor pagination has a problem.
-    Seeing that they both depend on the limit,
-    pagination distributes the item between pages depending on the limit and the total items.
-    Having 3 items from a search, and a limit of 5 causes the second page to have nothing because they're all on the first page!
-    The solution is to re-render the pagination buttons depending on the search results.
-    Meaning we update the total number of items state variable after every search.
-    */
-    this.setState({
-      searchTerm: event.target.value,
-      currentPage: 1 //this is a hack. needs a solution
+    store.dispatch({
+      type: 'SET_SEARCH_TERM',
+      payload: {
+        searchTerm: event.target.value,
+        currentPage: 1 //this is a hack. needs a solution
+      }
     });
   }
 
   render () {
     // render bucketlists
-    const rows = this.state.listToRender.map((item) =>
+    const rows = this.props.bucketlistState.listToRender.map((item) =>
       <Bucketlist key={item.id} bucketlistName={item.name} bucketlistId={item.id} getBucketlists={this.getBucketlists} />
     );
 
     // create pageButtons
     const pageButtons = [];
-    for (let i = 0; i < this.state.totalPages; i++) {
+    for (let i = 0; i < this.props.bucketlistState.totalPages; i++) {
       pageButtons.push(<span className="page_button" key={i+1} onClick={() => this.setCurrentPage(i+1)}>{i+1}</span>);
     }
 
     return (
       <div className="landing_page bucketlist_view">
         <Nav handleLogout={this.props.handleLogout} />
-        <Header currentLocation="Bucketlists" itemForm='/bucketlistform' searchTerm={this.state.searchTerm} handleSearch={this.handleSearch} />
+        <Header currentLocation="Bucketlists" itemForm='/bucketlistform' searchTerm={this.props.bucketlistState.searchTerm} handleSearch={this.handleSearch} />
         <Main componentToRender={rows} />
         <Footer pageButtons={pageButtons} />
       </div>
@@ -120,4 +109,10 @@ class BucketlistView extends Component {
   }
 }
 
-export default BucketlistView;
+const mapStateToProps = (store) => {
+  return {
+    bucketlistState: store.bucketlistView
+  };
+}
+
+export default connect(mapStateToProps)(BucketlistView);
