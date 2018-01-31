6c1855639e85164b22b601b45db100f8b0fbbc39
CK Wagaba <ckwagaba@gmail.com>
2018-01-31 15:13:11 +0300

    create item reducer

---
+++ src/reducers/ItemReducer.js
@@ -0,0 +1,44 @@
+const initialState = {
+  listToRender: [],
+  searchTerm: '',
+  bucketlistName: '',
+  totalItems: 0,
+  limit: 3,
+  currentPage: 1,
+  totalPages: 1
+}
+
+const itemReducer = (state=initialState, action) => {
+  switch(action.type) {
+    case 'STORE_ITEMS':
+      return {
+        ...state,
+        listToRender: action.payload.listToRender,
+        bucketlistName: action.payload.bucketlistName,
+        totalPages: action.payload.totalPages
+      }
+    case 'SET_TOTAL_ITEMS':
+      return {
+        ...state,
+        totalItems: action.payload
+      }
+
+    case 'SET_CURRENT_PAGE':
+      return {
+        ...state,
+        currentPage: action.payload
+      }
+
+    case 'SET_SEARCH_TERM':
+      return {
+        ...state,
+        searchTerm: action.payload.searchTerm,
+        currentPage: action.payload.currentPage
+      }
+
+    default:
+      return state;
+  }
+}
+
+export default itemReducer;
