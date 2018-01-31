fea78ca7737f7a2a8afc385272c4d5e3c4c5cba8
CK Wagaba <ckwagaba@gmail.com>
2018-01-31 15:10:12 +0300

    ensure uniqueness of ActionTypes

---
+++ src/actions/ActionTypes.js
@@ -0,0 +1,4 @@
+export const SET_TOTAL = 'SET_TOTAL';
+export const STORE_LIST = 'STORE_LIST';
+export const SET_SEARCH_TERM = 'SET_SEARCH_TERM';
+export const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
